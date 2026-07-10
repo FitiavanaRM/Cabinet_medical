e7a44a2ab479cfbe21f1c57a6ecd2646b3630058
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-10 17:05:52 +0300

    Creation du classe AppConfig

---
+++ src/main/java/com/cabinetmedical/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.cabinetmedical.config;
+
+public class AppConfig {
+    private String host;
+    private String port;
+    private String dbName;
+    private String username;
+    private String password;
+    private String params;
+
+    private static AppConfig instance;
+
+    public AppConfig() {
+    }
+
+}
