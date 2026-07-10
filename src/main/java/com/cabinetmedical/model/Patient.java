32a455285774e94e68a3ffc1292339ca253f955f
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-10 16:13:52 +0300

    Création du modèle Patient avec attributs et méthodes

--- src/main/java/com/cabinetmedical/model/Patient.java
+++ src/main/java/com/cabinetmedical/model/Patient.java
@@ -1,4 +1,95 @@
 package com.cabinetmedical.model;
 
+import java.time.LocalDate;
+
 public class Patient {
+    private int id;
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String telephone;
+    private String adresse;
+    private String antecedents;
+
+    public Patient() {
+    }
+
+    public Patient(int id, String nom, String prenom, LocalDate dateNaissance, String telephone, String adresse, String antecedents) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.telephone = telephone;
+        this.adresse = adresse;
+        this.antecedents = antecedents;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getAntecedents() {
+        return antecedents;
+    }
+
+    public void setAntecedents(String antecedents) {
+        this.antecedents = antecedents;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                ", telephone='" + telephone + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", antecedents='" + antecedents + '\'' +
+                '}';
+    }
 }
