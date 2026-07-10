3fd03a7775b0e16345c6e59dc06556804c667ec6
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-10 16:21:52 +0300

    Création du modèle Consultation avec attributs et méthodes

--- src/main/java/com/cabinetmedical/model/Consultation.java
+++ src/main/java/com/cabinetmedical/model/Consultation.java
@@ -1,4 +1,73 @@
 package com.cabinetmedical.model;
 
+import java.time.LocalDateTime;
+
 public class Consultation {
+    private int id;
+    private int idRdv;
+    private String diagnostic;
+    private String ordonnance;
+    private LocalDateTime dateConsultation;
+
+    public Consultation() {
+    }
+
+    public Consultation(int id, int idRdv, String diagnostic, String ordonnance, LocalDateTime dateConsultation) {
+        this.id = id;
+        this.idRdv = idRdv;
+        this.diagnostic = diagnostic;
+        this.ordonnance = ordonnance;
+        this.dateConsultation = dateConsultation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdRdv() {
+        return idRdv;
+    }
+
+    public void setIdRdv(int idRdv) {
+        this.idRdv = idRdv;
+    }
+
+    public String getDiagnostic() {
+        return diagnostic;
+    }
+
+    public void setDiagnostic(String diagnostic) {
+        this.diagnostic = diagnostic;
+    }
+
+    public String getOrdonnance() {
+        return ordonnance;
+    }
+
+    public void setOrdonnance(String ordonnance) {
+        this.ordonnance = ordonnance;
+    }
+
+    public LocalDateTime getDateConsultation() {
+        return dateConsultation;
+    }
+
+    public void setDateConsultation(LocalDateTime dateConsultation) {
+        this.dateConsultation = dateConsultation;
+    }
+
+    @Override
+    public String toString() {
+        return "Consultation{" +
+                "id=" + id +
+                ", idRdv=" + idRdv +
+                ", diagnostic='" + diagnostic + '\'' +
+                ", ordonnance='" + ordonnance + '\'' +
+                ", dateConsultation=" + dateConsultation +
+                '}';
+    }
 }
