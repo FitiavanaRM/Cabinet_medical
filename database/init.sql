17ea2a5075befc4ea225a6ae07b661f6220a1b3a
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-10 16:03:26 +0300

    insertion des donnees

--- database/init.sql
+++ database/init.sql
@@ -69,3 +69,24 @@ CREATE TABLE consultation (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+-- insertion donnees (test zani)
+INSERT INTO utilisateur (login, mot_de_passe, role) VALUES
+    ('admin', 'admin123', 'ADMIN'),
+    ('secretaire', 'secret123', 'SECRETAIRE'),
+    ('Dr Hery', 'medecin123', 'MEDECIN');
+
+INSERT INTO medecin (id_utilisateur, nom, prenom, specialite, telephone, email, jours_disponibles, heure_debut_dispo, heure_fin_dispo) VALUES
+    (NULL, 'Rotsy', 'Nomena', 'Medecin generale', '0389154855', 'rotsynomena9@gmail.com', 'Lundi, Mardi, Mercredi, Jeudi, Vendredi', '08:00:00', '18:00:00'),
+    (3, 'Hery', 'Joseph', 'Cardiologie', '0341326685', 'heryjoseph@gmail.com', 'Lundi, Mercredi, Vendredi', '09:00:00', '16:00:00');
+
+INSERT INTO patient (nom, prenom, date_naissance, telephone, adresse, antecedents) VALUES
+    ('Malalaniavo', 'Valisoa', '2005-02-03', '0381906779', 'Ambihibary', 'Asthme leger'),
+    ('Rakotomanantsoa', 'Yanki', '2007-01-08', '0344838065', 'Boys_Dorm', 'Aucun antecedent connu');
+
+INSERT INTO rendez_vous (id_patient, id_medecin, date_rdv, heure_rdv, motif, statut) VALUES
+    (1, 2, '2026-07-29', '15:00:00', 'Controle cardiage', 'PREVU'),
+    (2, 1, '2026-07-30', '10:00:00', 'Crise d asthme', 'HONORE');
+
+INSERT INTO consultation (id_rdv, diagnostic, ordonnance) VALUES
+    (2, 'Crise d asthme moderee', 'Ventoline 100mcg, 2 bouffees, maximum 4 fois par jour');
