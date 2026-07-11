d5c95c1288e83fd9462648965b37b21f04bfe571
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-11 20:30:45 +0300

    finalisation de DatabaseConnection utilisant AppConfig pour la connexion JSBC Mysql

--- src/main/java/com/cabinetmedical/util/DatabaseConnection.java
+++ src/main/java/com/cabinetmedical/util/DatabaseConnection.java
@@ -1,4 +1,20 @@
 package com.cabinetmedical.util;
 
+import com.cabinetmedical.config.AppConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Connexion JDBC vers Mysql
 public class DatabaseConnection {
+    public static Connection getConnection() throws SQLException {
+        AppConfig config = AppConfig.getInstance();
+
+        return DriverManager.getConnection(
+                config.buildJdbcUrl(),
+                config.getUsername(),
+                config.getPassword()
+        );
+    }
 }
