8fb25b3101db6684acbc7c096bd649494aefa6c3
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-15 11:03:09 +0300

    Ajout du code dans RendezVousService avec les methodes d'intrerdiction du rendez vous au passé

--- src/main/java/com/cabinetmedical/service/RendezVousService.java
+++ src/main/java/com/cabinetmedical/service/RendezVousService.java
@@ -3,6 +3,8 @@ package com.cabinetmedical.service;
 
 import com.cabinetmedical.dao.RendezVousDAO;
 import com.cabinetmedical.model.RendezVous;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class RendezVousService {
@@ -15,7 +17,7 @@ public class RendezVousService {
     }
 
     // rechercher un rdv par son Id
-    public RendezVous rechercheRendezVousParId(int id) throws Exception {
+    public List<RendezVous> rechercheRendezVousParId(int id) throws Exception {
         return rendezVousDAO.findAll();
     }
 
@@ -27,5 +29,30 @@ public class RendezVousService {
     // donne le liste rdv du medecin
     public List<RendezVous> listeRendezVousMedecin(int idMedecin) throws Exception {
         return rendezVousDAO.findByMedecin(idMedecin);
+    }
+
+    // creation d'un nouveau rendez vous
+    public void creerRdv(RendezVous rdv) throws Exception {
+
+    }
+
+    // validation
+    private void validationRdv(RendezVous rdv) throws Exception {
+        if (rdv.getIdPatient() <= 0) {
+            throw new Exception("Le patient est obligatoire");
+        }
+
+        if (rdv.getIdMedecin() <= 0) {
+            throw new Exception("Le medecin est obligatoire");
+        }
+
+        if (rdv.getDateRdv() == null || rdv.getHeureRdv() == null) {
+            throw new Exception("La date et l'heure du rendez vous sont obligatoire");
+        }
+
+        LocalDateTime dateHeureRdv = LocalDateTime.of(rdv.getDateRdv(), rdv.getHeureRdv());
+        if (dateHeureRdv.isBefore(LocalDateTime.now())) {
+            throw new Exception("Impossible de creer un rendez vous dans le passé");
+        }
     }
 }
