bcdcd54da4986df1ebfe1d281acc2692d18a8ff8
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-15 10:42:40 +0300

    Ajout du code dans RendezVousService avec les methodes pour lister et rechercher tous les Rendez Vous, et lister le rdv du patient

--- src/main/java/com/cabinetmedical/service/RendezVousService.java
+++ src/main/java/com/cabinetmedical/service/RendezVousService.java
@@ -1,4 +1,28 @@
 package com.cabinetmedical.service;
 
+
+import com.cabinetmedical.dao.RendezVousDAO;
+import com.cabinetmedical.model.RendezVous;
+import java.util.List;
+
 public class RendezVousService {
+    private RendezVousDAO rendezVousDAO = new RendezVousDAO();
+    private MedecinService medecinService = new MedecinService();
+
+    // liste de tous les rdv
+    public List<RendezVous> listeRdv() throws Exception {
+        return rendezVousDAO.findAll();
+    }
+
+    // rechercher un rdv par son Id
+    public RendezVous rechercheRendezVousParId(int id) throws Exception {
+        return rendezVousDAO.findAll();
+    }
+
+    // donne le rendez vous du patient
+    public List<RendezVous> listeRendezVousPatient(int idPatient) throws Exception {
+        return rendezVousDAO.findByPatient(idPatient);
+    }
+
+    //
 }
