c980ed7cf87eb1d9da69bfd848d9ce6a22efc039
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-29 08:15:50 +0300

    Ajout du code dans alertUtil

--- src/main/java/com/cabinetmedical/util/AlertUtil.java
+++ src/main/java/com/cabinetmedical/util/AlertUtil.java
@@ -1,4 +1,26 @@
 package com.cabinetmedical.util;
 
+import javafx.scene.control.Alert;
+
+// classe qui affiche message d'alerte
+
 public class AlertUtil {
+
+    // Affiche un message d'erreur
+    public static void afficherErreur(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erreur");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    // Affiche un message de succes
+    public static void afficherSucces(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Succes");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
