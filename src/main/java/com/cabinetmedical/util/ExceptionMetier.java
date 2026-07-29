b8fdbb4524fac715dfbc529bb27cd1f2d39b98bc
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-29 23:31:28 +0300

    Ajout du ExceptionMetier pour dediee aux erreur de validation

--- src/main/java/com/cabinetmedical/util/ExceptionMetier.java
+++ src/main/java/com/cabinetmedical/util/ExceptionMetier.java
@@ -1,4 +1,24 @@
 package com.cabinetmedical.util;
 
-public class ExceptionMetier {
+// Exception métier dédiée aux erreurs de validation ou de logique métier.
+
+public class ExceptionMetier extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ExceptionMetier() {
+        super();
+    }
+
+    public ExceptionMetier(String message) {
+        super(message);
+    }
+
+    public ExceptionMetier(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ExceptionMetier(Throwable cause) {
+        super(cause);
+    }
 }
